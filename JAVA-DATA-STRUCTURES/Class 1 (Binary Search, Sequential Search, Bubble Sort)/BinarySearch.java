ef283a143b47a543cfb668f5dc78dcc092a6f999
hamzapro305 <hamzapro285@gmail.com>
2022-08-21 14:00:17 +0500

    Require Syntax Changes And Error Fixing

--- JAVA-DATA-STRUCTURES/Class 1 (Binary Search, Sequential Search, Bubble Sort)/BinarySearch.java
+++ JAVA-DATA-STRUCTURES/Class 1 (Binary Search, Sequential Search, Bubble Sort)/BinarySearch.java
@@ -1,37 +1,34 @@
 package DSA.Sorting;
 
-import java.nio.ReadOnlyBufferException;
 import java.util.Scanner;
 
 public class BinarySearch {
 
-    public static Boolean search(int[]arr,int a) {
+    public static Boolean search(int[] arr, int a) {
         int sp = 0;
         int ep = arr.length;
-        int mid=(sp+ep)/2;
+        int mid = (sp + ep) / 2;
 
         for (int i = 0; i < arr.length; i++) {
-            if (a<arr[mid]) {
-                ep=mid;
-                mid=(sp+ep)/2;
-            }
-            else if (a>arr[mid]) {
-                sp=mid;
-                mid=(sp+ep)/2;
-            }
-            else{
+            if (a < arr[mid]) {
+                ep = mid;
+                mid = (sp + ep) / 2;
+            } else if (a > arr[mid]) {
+                sp = mid;
+                mid = (sp + ep) / 2;
+            } else {
                 return true;
             }
         }
         return false;
     }
-    
+
     public static void main(String[] args) {
-        int[] arr= {0,2,4,6,8,10};
+        int[] arr = { 0, 2, 4, 6, 8, 10 };
         Scanner scan = new Scanner(System.in);
         System.out.print("Enter Number : ");
-        int a=scan.nextInt();
-        System.out.println(search(arr,a));
-
+        int a = scan.nextInt();
+        System.out.println(search(arr, a));
+        scan.close();
     }
 }
