a0023941da0e4094241ce59f09f8199c492603f6
Hamza Siddiqui <hamzapro285@gmail.com>
2022-08-21 18:36:32 +0500

    Setting to main
    Setting to main

---
+++ JAVA-DATA-STRUCTURES/Class 1 (Binary Search, Sequential Search, Bubble Sort)/BubbleSort.java
@@ -0,0 +1,32 @@
+package DSA.Sorting;
+
+public class BubbleSort {
+
+    public static void bubbleSort(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+
+            for (int j = 0; j < arr.length - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+        printArray(arr);
+    }
+
+    public static void printArray(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(" " + arr[i]);
+        }
+        System.out.println("\n");
+    }
+
+    public static void main(String[] args) {
+
+        int[] arr = { 6, 3, 2, 5, 4, 1 };
+        bubbleSort(arr);
+
+    }
+}
---
+++ JAVA-DATA-STRUCTURES/Class 1 (Binary Search, Sequential Search, Bubble Sort)/SequentialSearch.java
@@ -0,0 +1,24 @@
+package DSA.Sorting;
+
+import java.util.Scanner;
+
+public class SequentialSearch {
+
+    public static Boolean search(int[] arr, int s) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == s) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 0, 2, 4, 6, 8, 10 };
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Number : ");
+        int s = sc.nextInt();
+        System.out.println(search(arr, s));
+        sc.close();
+    }
+}
